2f8f8e42d7670c3960c49e72172e2128a0011b68
riccardo <r.giambra@jetop.com>
2021-04-22 12:32:51 +0200

    esercizio delle lezioni

---
+++ esercizio_lista/script.js
@@ -0,0 +1,43 @@
+
+
+let json;
+let requestURL="./lista.json";
+/*
+let request=new XMLHttpRequest();
+request.open("GET",requestURL)
+request.responseType="json";   
+request.send()
+request.onload = ()=>{
+    
+    console.log("fatto")
+}
+*/
+
+fetch(requestURL)
+    .then(response=>response.json())
+    .then(ciao=>{
+        json=ciao;
+        console.log(json);
+    })
+
+
+
+
+bottone=document.querySelector('button');
+bottone.textContent="premimi";
+bottone.addEventListener('click',()=>
+{
+
+    //json=request.response;
+
+    for(let i=0;i<3;i++){
+    let img=document.createElement('img');
+    img.setAttribute("src",json.lista[i].immagine)
+    img.setAttribute("width","100px");
+    document.body.append(img);
+    let txt=document.createElement('p');
+    txt.textContent= `${json.lista[i].prodotto} ${json.lista[i].descrizione} ${json.lista[i].prezzo}`;
+    document.body.append(txt);
+    
+}
+})
